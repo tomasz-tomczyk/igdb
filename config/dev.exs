bf97e1fec5da0985bfb7baea5d9814ed09b8cc8c
Tomasz Tomczyk <me@tomasztomczyk.com>
2018-01-22 22:05:31 +0000

    Initialise config setup

---
+++ config/dev.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+
+config :igdb, api_key: {:system, "IGDB_API_KEY"}
---
+++ config/test.exs
@@ -0,0 +1,5 @@
+use Mix.Config
+
+config :igdb, api_key: {:system, "IGDB_TEST_API_KEY"}
+
+config :logger, level: :info
